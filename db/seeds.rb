5d002eb246ba1a7762c98000c6ff2bf6534d468d
Scott Weinman <itsmymacguy@gmail.com>
2021-04-12 17:18:21 -0400

    db/seeds.rb attempted to fix model name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Smoothiechoices.create(name: 'Caribbean Way', description: 'Bananas, Strawberries, Papaya Juice Blend, Turbinado', photo: 'CaribbeanWay.png')
-Smoothiechoices.create(name: 'Pineapple Surf', description: 'Strawberries, Pineapples, Kiwi Apple Juice Blend, Turbinado, Dairy Whey Blend', photo: 'PineappleSurf.png')
-Smoothiechoices.create(name: 'Apple Kiwi Bunga', description: 'Bananas, Kale - Organic, Apple Juice, Kiwi Apple Juice Blend, Multivitamin Enhancer', photo: 'AppleKiwi.png')
-Smoothiechoices.create(name: 'Island Impact', description: 'Pineapples, Mangoes, Papaya Juice Blend, Apple Pineapple Juice Blend, Protein Blend', photo: 'IslandImpact.png')
+Smoothiechoice.create(name: 'Caribbean Way', description: 'Bananas, Strawberries, Papaya Juice Blend, Turbinado', photo: 'CaribbeanWay.png')
+Smoothiechoice.create(name: 'Pineapple Surf', description: 'Strawberries, Pineapples, Kiwi Apple Juice Blend, Turbinado, Dairy Whey Blend', photo: 'PineappleSurf.png')
+Smoothiechoice.create(name: 'Apple Kiwi Bunga', description: 'Bananas, Kale - Organic, Apple Juice, Kiwi Apple Juice Blend, Multivitamin Enhancer', photo: 'AppleKiwi.png')
+Smoothiechoice.create(name: 'Island Impact', description: 'Pineapples, Mangoes, Papaya Juice Blend, Apple Pineapple Juice Blend, Protein Blend', photo: 'IslandImpact.png')

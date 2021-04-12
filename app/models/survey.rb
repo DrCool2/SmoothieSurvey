48669e965ad3e878a9a5257b9a1dd430901f9f78
Scott Weinman <itsmymacguy@gmail.com>
2021-04-12 17:35:14 -0400

    Survey - undo the custom message

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,7 @@
 class Survey < ApplicationRecord
   belongs_to :smoothiechoice
 
-  validates :user_id, uniqueness: true, message: "You may only submit ONE survey!"
+  validates :user_id, uniqueness: true
 
 def self.survey_results
 

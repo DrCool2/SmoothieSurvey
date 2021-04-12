05efe96d981752b268772534a03d8a145734b933
Scott Weinman <itsmymacguy@gmail.com>
2021-04-12 18:12:41 -0400

    config/environments/production.rb - added smoothie.fsdbk12.org as an acceptable host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.hosts << "smoothiesurvey.fsdbk12.org"
+  config.hosts << "smoothie.fsdbk12.org"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
